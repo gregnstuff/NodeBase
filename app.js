dc464edf04d7d02c3d832f3f15559d4ef70cc6bd
gregnstuff <gshort@shortfam.com>
2024-05-18 13:23:21 -0500

    Set static files, view engine and add security middleware to app.js

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const path = require("path");
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const app = express();
+
+// Set Security HTTP headers
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// Serving static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: ["quanity"],
+  })
+);
 
 module.exports = app;

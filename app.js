a19a9c8e876d73b1bb9c4471ec6f932fdcc79577
gregnstuff <gshort@shortfam.com>
2024-05-21 06:38:48 -0500

    Add body parser to read data from body into req.body

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ const limiter = rateLimit({
 });
 app.use("/api", limiter);
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
 // Data Sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
